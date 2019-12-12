bd3f953dd25a129cf7d6c974a48c57f70fed8d38
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2019-12-12 21:44:15 +0300

    add package.json, fix minor issues

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -2,19 +2,19 @@ import { format } from './utils';
 
 describe('format', () => {
   it('returns empty string for no names defined', () => {
-    expect(format(undefined, undefined, undefined)).toEqual('');
+    expect(format(undefined, undefined, undefined, undefined)).toEqual('');
   });
 
   it('formats just first names', () => {
-    expect(format('Joseph', undefined, undefined)).toEqual('Joseph');
+    expect(format('Mr.','Joseph', undefined, undefined)).toEqual('Joseph');
   });
 
   it('formats first and last names', () => {
-    expect(format('Joseph', undefined, 'Publique')).toEqual('Joseph Publique');
+    expect(format(undefined,'Joseph', undefined, 'Publique')).toEqual('Joseph Publique');
   });
 
   it('formats first, middle and last names', () => {
-    expect(format('Joseph', 'Quincy', 'Publique')).toEqual(
+    expect(format(undefined,'Joseph', 'Quincy', 'Publique')).toEqual(
       'Joseph Quincy Publique'
     );
   });
